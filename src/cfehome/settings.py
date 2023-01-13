cd76040d5428159284abb14e9a5bd38022d92381
CodingForEntrepreneurs <hello@teamcfe.com>
2023-01-13 10:32:23 -0600

    5 - Static File Configuration for Django

--- src/cfehome/settings.py
+++ src/cfehome/settings.py
@@ -116,6 +116,14 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+]
+STATIC_ROOT = BASE_DIR.parent / "local-cdn" / "static"
+MEDIA_URL = 'media/'
+MEDIA_ROOT = BASE_DIR.parent / "local-cdn" / "media"
+
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
--- src/cfehome/urls.py
+++ src/cfehome/urls.py
@@ -13,9 +13,15 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf import settings
 from django.contrib import admin
 from django.urls import path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
 ]
+
+if settings.DEBUG:
+    from django.conf.urls.static import static
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
