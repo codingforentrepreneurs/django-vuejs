9021cc34376d6eebd1455260410ec5010dbc1ff3
CodingForEntrepreneurs <hello@teamcfe.com>
2023-01-18 10:17:52 -0600

    19 - Handling HTML Forms in Vue

--- frontend/components/CreateForm.vue
+++ frontend/components/CreateForm.vue
@@ -17,7 +17,11 @@ const handleSubmitForm = async (event) => {
     if (event) {
         event.preventDefault()
     }
+    const target = event.target
+    const innerFormData = new FormData(target)
+    const innerFormDataJson = JSON.stringify(Object.fromEntries(innerFormData))
     const formDataJson= JSON.stringify(formData)
+    console.log(innerFormDataJson, formDataJson)
     const csrfToken = store.token
     const axiosConfig = {headers: {"X-CSRFToken": csrfToken}}
     let response;
@@ -33,20 +37,25 @@ const handleSubmitForm = async (event) => {
 </script>
 
 <template>
-    <div>
+    <form method="POST" action="/django/" @submit.prevent="handleSubmitForm">
         <div>
-        <input type="text" v-model="formData.title" name="title" placeholder="Your blog title" v-on:keyup="handleAutoSlugChange"/>
+            <div>
+            <input type="text" required v-model="formData.title" name="title" placeholder="Your blog title" v-on:keyup="handleAutoSlugChange"/>
 
-    </div>
-    <div>
-        <input type="text" v-model="formData.slug" name="slug" placeholder="your-blog-slug"/>
+        </div>
+        <div>
+            <input type="text" required v-model="formData.slug" name="slug" placeholder="your-blog-slug"/>
 
-    </div>
-    <div>
-        <p>Preview:</p>
-        <p>Title: {{ formData.title  }}</p>
-        <p>Slug: {{ formData.slug  }}</p>
-        <button @click="handleSubmitForm">Send</button>
-    </div>
-    </div>
+        </div>
+        <div>
+            <input type="text" required v-model="formData.slugger" name="slugger" placeholder="your-blog-slug"/>
+        </div>
+        <div>
+            <p>Preview:</p>
+            <p>Title: {{ formData.title  }}</p>
+            <p>Slug: {{ formData.slug  }}</p>
+            <button type="submit">Send</button>
+        </div>
+        </div>
+    </form>
 </template>
